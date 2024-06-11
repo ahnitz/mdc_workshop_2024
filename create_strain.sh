2c48624fb384cac35558e425bfbfa7a5d9fa1446
Alexander Harvey Nitz <alex.nitz@gmail.com>
2024-06-11 01:10:28 -0400

    update

--- create_strain.sh
+++ create_strain.sh
@@ -1,8 +1,8 @@
 export PYCBC_DETECTOR_CONFIG=$PWD/custom_detectors.ini
 
 pycbc_condition_strain \
---gps-start-time 179998000 \
---gps-end-time 180001000 \
+--gps-start-time 1799998000 \
+--gps-end-time   1800001000 \
 --pad-data 8 \
 --fake-strain-from-file ce_strain/cosmic_explorer_strain.txt \
 --fake-strain-seed 0 \
