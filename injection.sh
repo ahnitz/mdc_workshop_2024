710064833229bd49cbf83919a6f44c3bda6c5644
Alexander Harvey Nitz <alex.nitz@gmail.com>
2024-06-10 23:21:25 -0400

    add initial example

---
+++ injection.sh
@@ -0,0 +1,7 @@
+pycbc_create_injections --verbose \
+        --config-files injection.ini \
+        --ninjections 1 \
+        --seed 10 \
+        --output-file injection.hdf \
+        --force
+
---
+++ run.sh
@@ -0,0 +1,29 @@
+export PYCBC_DETECTOR_CONFIG=$PWD/custom_detectors.ini
+export OMP_NUM_THREADS=1
+
+pycbc_inference \
+--config-file reltime.ini \
+--nprocesses 2 \
+--output-file reltime.hdf \
+--seed 0 \
+--force \
+--verbose
+
+
+# This reconstructs any marginalized parameters
+# and would be optional if you don't need them or
+# have sampled over all parameters directly (see reltime.ini)
+OMP_NUM_THREADS=1 python -m cProfile -o log2 `which pycbc_inference_model_stats` \
+--input-file reltime.hdf \
+--output-file reltime_demarg.hdf \
+--nprocesses 2 \
+--reconstruct-parameters \
+--force \
+--verbose
+
+pycbc_inference_plot_posterior \
+--input-file reltime_demarg.hdf \
+--output-file reltime.png \
+--parameters distance inclination polarization coa_phase tc ra dec mchirp eta \
+--plot-injection-parameters \
+--z-arg snr
